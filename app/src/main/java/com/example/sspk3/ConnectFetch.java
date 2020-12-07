57b1a99684e33b9b809fed843141e9dfc81bb788
ikli21 <sergej.sergeevbo@mail.ru>
2020-12-07 23:55:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sspk3/ConnectFetch.java
@@ -0,0 +1,104 @@
+package com.example.sspk3;
+
+import android.content.Context;
+import android.os.Handler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConnectFetch {
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
+    private static final String OPEN_WEATHER_ICON ="http://openweathermap.org/img/wn/%s@2x.png";
+    private Handler handler;
+    private OnConnectionCompleteListener listener;
+
+    public ConnectFetch(Context context, String city, OnConnectionCompleteListener listener)
+    {
+        this.listener = listener;
+        handler = new Handler();
+        updateWeatherData(city,context);
+    }
+
+    private void updateWeatherData(final String city, final Context context){
+        new Thread(){
+            public void run(){
+                final JSONObject json = ConnectFetch.getJSON(context, city);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            listener.onFail(city);
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                            listener.onSuccess(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+    //Praktika 3 session 4 str 4s
+
+
+    public static JSONObject getJSON(Context context, String city){
+        try {
+            String urlString = String.format(OPEN_WEATHER_MAP_API, city,context.getString(R.string.weather_api_key));
+            URL url = new URL(urlString);
+
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+//            connection.addRequestProperty("x-api-key",
+//                    context.getString(R.string.weather_api_key));
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+    public static String getIconUrl(JSONObject json)
+    {
+        try {
+
+//          первый элемент массива метеорологических данных.
+            JSONObject details =  json.getJSONArray("weather").getJSONObject(0) ;
+            String icon = details.getString("icon");
+            return String.format(OPEN_WEATHER_ICON, icon);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    public interface OnConnectionCompleteListener {
+        void onSuccess(JSONObject response);
+        void onFail(String message);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/sspk3/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.sspk3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONObject;
+
+import static com.example.sspk3.ConnectFetch.getIconUrl;
+import static com.example.sspk3.StaticWeatherAnalyze.getCityField;
+import static com.example.sspk3.StaticWeatherAnalyze.getDetailsField;
+import static com.example.sspk3.StaticWeatherAnalyze.getLastUpdateTime;
+import static com.example.sspk3.StaticWeatherAnalyze.getTemperatureField;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new  ConnectFetch(this, "Orenburg", new     ConnectFetch.OnConnectionCompleteListener() {
+            @Override
+            public void onSuccess(JSONObject response) {
+                renderWeather(response);
+            }
+
+            @Override
+            public void onFail(String message) {
+
+                Toast.makeText(MainActivity.this,
+                        message,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void updateWeatherData(final String city){
+        new Thread(){
+            public void run(){
+                final JSONObject json = ConnectFetch.getJSON(MainActivity.this, city);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            Toast.makeText(MainActivity.this,
+                                    city + "-информация не найдена",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json){
+        try {
+
+//            Ответы:
+//                      json.getString("name") - название города
+//                      json.getJSONObject("sys").getString("country") - название страны
+//                      JSONObject details =  json.getJSONArray("weather").getJSONObject(0) - первый элемент массива метеорологических данных.
+//                           details.getInt("id")  - идентификатор погоды\
+//                           details.getString("description") - краткое описание погоды
+
+//                      JSONObject main = json.getJSONObject("main"); - узел main
+//                          main.getString("humidity")  - влажность
+//                          main.getString("pressure")  - давление
+//                            main.getDouble("temp")    - температура
+
+//                      DateFormat df = DateFormat.getDateTimeInstance();
+//                      String updatedOn = df.format(new Date(json.getLong("dt")*1000)); - время получения информации системой
+
+//                      json.getJSONObject("sys").getLong("sunrise") - время восхода
+//                      json.getJSONObject("sys").getLong("sunset") - время заката
+
+
+
+//            JSONObject details =  json.getJSONArray("weather").getJSONObject(0);
+//            ((TextView)findViewById(R.id.weather)).setText(details.getString("description").toUpperCase());
+            Glide
+                    .with(this)
+                    .load(getIconUrl(json))
+                    .into((ImageView)findViewById(R.id.weather_icon));
+            ((TextView)findViewById(R.id.city_field)).setText(getCityField(json));
+            ((TextView)findViewById(R.id.updated_field)).setText(getLastUpdateTime(json));
+            ((TextView)findViewById(R.id.details_field)).setText(getDetailsField(json));
+            ((TextView)findViewById(R.id.current_temperature_field)).setText(getTemperatureField(json));
+
+        }catch(Exception e){
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+
+    }
+
+
+}
